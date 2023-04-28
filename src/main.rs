3e7af9c9084279b98d25df955b0680a81c5f0eef
Michael Hume <michael.a.hume@pm.me>
2023-04-27 22:53:15 -0400

    feat(api): - Setup endpoint for health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+#[tokio::main]
+async fn main() -> Result<(), io::Error> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
